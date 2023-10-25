81fc9956519c9d808d88cbf5f20b667c79151df5
Joshua Burkholder <ehlodex@users.noreply.github.com>
2023-10-25 16:09:05 -0400

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,6 +1,6 @@
 <#
   .SYNOPSIS
-  Bootstraps a Windows 10 PC with common applications and settings
+  Bootstraps a Windows 10/11 PC with common applications and settings
   
   .DESCRIPTION
     The bootstrap.ps1 script contains the basic software components for a manual deployment.
@@ -16,7 +16,7 @@
     https://technet.microsoft.com/en-us/library/hh847834.aspx
   
   .NOTES
-    Updated 2020.11 by: Joshua Burkholder [ehlodex]
+    Updated 2023.10 by: Joshua Burkholder [ehlodex]
 #>
 
 [CmdletBinding()] Param( [switch]$Restart );
@@ -31,27 +31,28 @@ $WindowsIdentity=[System.Security.Principal.WindowsIdentity]::GetCurrent()
 $WindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($WindowsIdentity)
 $Administrator=[System.Security.Principal.WindowsBuiltInRole]::Administrator
 $Elevated=$WindowsPrincipal.IsInRole($Administrator)
-If (! $Elevated) { Write-Error "Administrative rights required! Please elevate PowerShell then try again."; exit; }
+If (! $Elevated) { Write-Error "Administrative rights required! Please elevate PowerShell then try again."; Exit 1; }
 
 Write-Verbose "Setting script-specific variables"                                                                               # # Variables
 $RestartDelayInSeconds = 30
-$PowerShellArguments = "ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=0 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1"
-$SchTaskAction   = New-ScheduledTaskAction -Execute 'choco.exe' -Argument 'upgrade all -y' -WorkingDirectory "C:\ProgramData\chocolatey"
-$SchTaskTrigger  = New-ScheduledTaskTrigger -Daily -At 2am
-$SchTaskSettings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Hours 2) -DontStopIfGoingOnBatteries -StartWhenAvailable
 $RemoveFeatures     = @("Printing-Foundation", "Printing-XPSServices", "WorkFolders-Client")
 $RemoveCapabilities = @("Browser.InternetExplorer", "Hello.Face", "SNMP.Client", "WMI-SNMP-Provider.Client", "XPS.Viewer")
-$RemoveAppxPackages = @("GetStarted", "Microsoft3DViewer", "MicrosoftOfficeHub", "MixedReality", "OneConnect", "OneNote", "Print3D", "SkypeApp", "WindowsCommunicationsApps", "WindowsFeedbackHub", "Xbox", "ZuneMusic")
+$RemoveAppxPackages = @("GetStarted", "Microsoft3DViewer", "MicrosoftOfficeHub", "MicrosoftTeams", "MixedReality", "OneConnect", "OneNote", "Print3D", "SkypeApp", "WindowsCommunicationsApps", "WindowsFeedbackHub", "Xbox", "ZuneMusic")
 $RemovePrinters     = @("OneNote", "Fax")
+$RemoveLocalUsers   = @("Setup User", "tempadmin", "localuser")
 
+<# Chocolatey
 Write-Verbose "Preparing to install Chocolatey and refresh the PowerShell environment"                                          # # Install Chocolatey + Apps
 Set-ExecutionPolicy Bypass -Scope Process -Force -Verbose:$PSVE -Debug:$PSDE
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); refreshenv
-  choco install -y powershell-core --install-arguments="$PowerShellArguments"
   choco install -y 7zip
   choco install -y vlc
 
-Write-Verbose "Creating Scheduled Task for Chocolaty Unattended Upgrades"                                                       # # Scheduled Task
+$SchTaskAction   = New-ScheduledTaskAction -Execute 'choco.exe' -Argument 'upgrade all -y' -WorkingDirectory "C:\ProgramData\chocolatey"
+$SchTaskTrigger  = New-ScheduledTaskTrigger -Daily -At 2am
+$SchTaskSettings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Hours 2) -DontStopIfGoingOnBatteries -StartWhenAvailable
+
+Write-Verbose "Creating Scheduled Task for Chocolaty Unattended Upgrades"                                                       # # Chocolatey Scheduled Task
 Register-ScheduledTask -User SYSTEM -RunLevel Highest -Verbose:$PSVE -Debug:$PSDE `
   -Action $SchTaskAction `
   -Trigger $SchTaskTrigger `
@@ -59,11 +60,12 @@ Register-ScheduledTask -User SYSTEM -RunLevel Highest -Verbose:$PSVE -Debug:$PSD
   -TaskPath "CUSTOM" `
   -TaskName "Chocolatey Upgrade Daemon" `
   -Description "Unattended upgrade for chocolatey apps"
+#>
+
 
 Write-Verbose "Adding registry key for `'Console lock display off timeout`' in `'Power Options`'"                               # # Lock screen timeout (regedit)
 New-ItemProperty -Name "Attributes" -Value 2 -PropertyType "Dword" -Force `
   -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\7516b95f-f776-4464-8c53-06167f40cc99\8EC4B3A5-6868-48c2-BE75-4F3044BE88A7"
-
 
 Write-Verbose "Preparing to remove unwanted Windows Optional Features"                                                          # # windows Optional Features
 ForEach ($Feature in $RemoveFeatures) {
@@ -89,18 +91,34 @@ ForEach ($Package in $RemoveAppxPackages) {
       Remove-AppxPackage -AllUsers -Confirm:$FALSE -Verbose:$PSVE -Debug:$PSDE -ErrorAction SilentlyContinue
 }
 
-# Remove AppX alternate Plan. Remove all apps, re-install the windows Store
-# Get-AppXProvisionedPackage -Online | Remove-AppxProvisionedPackage -Online -Verbose:$PSVE -Debug:$PSDE -ErrorAction SilentlyContinue
-# Get-AppXPackage -AllUsers | Remove-AppxPackage -AllUsers -Confirm:$FALSE -Verbose:$PSVE -Debug:$PSDE -ErrorAction SilentlyContinue
-# Get-AppxPackage -AllUsers Microsoft.WindowsStore | ForEach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
-
 Write-Verbose "Preparing to remove unwanted built-in printers"                                                                  # # Remove Printers
 ForEach ($Printer in $RemovePrinters) {
   Remove-Printer -Name "*$Printer*" -Confirm:$FALSE -Verbose:$PSVE -Debug:$PSDE
 }
 
+Write-Verbose "Cleaning up local accounts"                                                                                      # # Local Account Cleanup
+ForEach ($Username in $RemoveLocalUsers) {
+  $LocalUser = Get-LocalUser $Username -ErrorAction SilentlyContinue
+  # If the user exists locally...
+  If ($LocalUser) {
+    $UserRegistryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$($LocalUser.SID)"
+    # Delete the profile data from registry
+    If ((Test-Path $UserRegistryPath) -and ($UserRegistryPath -ne "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\")) {
+      $UserProfilePath = Get-ItemPropertyValue $UserRegistryPath ProfileImagePath
+      Remove-Item $UserRegistryPath -Force -Confirm:$False
+    }
+    # Delete the user profile folder (usually at C:\Users\)
+    If ((Test-Path variable:UserProfilePath) -and (Test-Path $UserProfilePath)) {
+      Get-ChildItem -Path $UserProfilePath -Recurse | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+      Remove-Item $UserProfilePath -Recurse -Force -ErrorAction SilentlyContinue
+    }
+    # Remove the local user account
+    $LocalUser | Remove-LocalUser -Confirm:$False
+  }
+}
+
 Set-Volume -DriveLetter C -NewFileSystemLabel "$($env:ComputerName)"
-Remove-Item -Path "C:\Apps\bootstrap.ps1" -ErrorAction SilentlyContinue -Verbose:$PSVE -Debug:$PSDE
+# Remove-Item -Path "C:\Apps\bootstrap.ps1" -ErrorAction SilentlyContinue -Verbose:$PSVE -Debug:$PSDE
 
 If ($Restart) { 
   Write-Warning "Computer will restart in $RestartDelayInSeconds seconds!"
@@ -109,3 +127,5 @@ If ($Restart) {
 } else {
   Write-Warning "Please reboot this computer to finalize changes made by this script."
 }
+
+Exit 0
