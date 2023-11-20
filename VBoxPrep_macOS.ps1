6d605f6c41537ec5e9157b2280d68166075950ef
Joshua Burkholder <ehlodex@users.noreply.github.com>
2023-11-20 15:14:34 -0500

    Create VBoxPrep_macOS.ps1

---
+++ VBoxPrep_macOS.ps1
@@ -0,0 +1,20 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True,Position=1,ValueFromPipeline=$True)]
+    [string]$VM,
+    [Parameter(Mandatory=$False,Position=2,ValueFromPipeline=$True)]
+    [string]$GuestResolution="1440x900",
+    [Parameter(Mandatory=$False,Position=3,ValueFromPipeline=$True)]
+    [string]$VboxManage="C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
+)
+
+Set-StrictMode -Version Latest
+
+& $VBoxManage modifyvm "$VM" --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
+& $VBoxManage setextradata "$VM" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "iMac19,1"
+& $VBoxManage setextradata "$VM" "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
+& $VBoxManage setextradata "$VM" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Mac-AA95B1DDAB278B95"
+& $VBoxManage setextradata "$VM" "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+& $VBoxManage setextradata "$VM" "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
+& $VBoxManage setextradata "$VM" "VBoxInternal/TM/TSCMode" "RealTSCOffset"
+& $VBoxManage setextradata "$VM" "VBoxInternal2/EfiGraphicsResolution" "$RES"
